26e54fa648de285bcaaf4eee27d97b4b807d24c6
Stalin Caiche Morán <stan@stancodes.localdomain>
2023-04-07 00:17:45 -0500

    Servicio CRUD de categories

--- src/main/java/com/stancodes/inventory/controller/CategoryRestController.java
+++ src/main/java/com/stancodes/inventory/controller/CategoryRestController.java
@@ -2,10 +2,16 @@ package com.stancodes.inventory.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.stancodes.inventory.model.Category;
 import com.stancodes.inventory.response.CategoryResponseRest;
 import com.stancodes.inventory.services.ICategoryService;
 
@@ -16,10 +22,68 @@ public class CategoryRestController {
     @Autowired
     private ICategoryService service;
 
+    /**
+     * Get all the categories 
+     * @return
+     */
     @GetMapping("/categories")
     public ResponseEntity<CategoryResponseRest> searchCategories() {
 
         ResponseEntity<CategoryResponseRest> response = service.search();
+
+        return response;
+    }
+
+    /**
+     * Get categories by id
+     * 
+     * @param id
+     * @return
+     */
+    @GetMapping("/categories/{id}")
+    public ResponseEntity<CategoryResponseRest> searchCategoriesById(@PathVariable Long id) {
+
+        ResponseEntity<CategoryResponseRest> response = service.searchById(id);
+
+        return response;
+    }
+
+    /**
+     * Save categories 
+     * 
+     * @return
+     */
+    @PostMapping("/categories")
+    public ResponseEntity<CategoryResponseRest> save(@RequestBody Category category) {
+
+        ResponseEntity<CategoryResponseRest> response = service.save(category);
+
+        return response;
+    }
+
+    /**
+     * Update category
+     * @param category
+     * @param id
+     * @return
+     */
+    @PutMapping("/categories/{id}")
+    public ResponseEntity<CategoryResponseRest> update(@RequestBody Category category, @PathVariable Long id) {
+
+        ResponseEntity<CategoryResponseRest> response = service.update(category, id);
+
+        return response;
+    }
+
+    /**
+     * Delete category
+     * @param id
+     * @return
+     */
+    @DeleteMapping("/categories/{id}")
+    public ResponseEntity<CategoryResponseRest> deleteById(@PathVariable Long id) {
+
+        ResponseEntity<CategoryResponseRest> response = service.deleteById(id);
 
         return response;
     }
--- src/main/java/com/stancodes/inventory/services/ICategoryService.java
+++ src/main/java/com/stancodes/inventory/services/ICategoryService.java
@@ -2,9 +2,14 @@ package com.stancodes.inventory.services;
 
 import org.springframework.http.ResponseEntity;
 
+import com.stancodes.inventory.model.Category;
 import com.stancodes.inventory.response.CategoryResponseRest;
 
 public interface ICategoryService {
     
     public ResponseEntity<CategoryResponseRest> search();
+    public ResponseEntity<CategoryResponseRest> searchById(Long id);
+    public ResponseEntity<CategoryResponseRest> save(Category category);
+    public ResponseEntity<CategoryResponseRest> update(Category category, Long id);
+    public ResponseEntity<CategoryResponseRest> deleteById(Long id);
 }
